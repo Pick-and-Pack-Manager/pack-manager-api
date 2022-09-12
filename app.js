2c5f4cf2607d682fab98c0fc8beed828a9e7dd7b
kylejkent <kylejkent@gmail.com>
2022-09-12 14:32:20 +0700

    add Results Render to Apps

--- app.js
+++ app.js
@@ -46,6 +46,9 @@ app.get('/', (req, res) => {
   res.render('search')
 })
 // Create route for results
+app.get('/results', (req, res) => {
+  res.render('results')
+})
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
