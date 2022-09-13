68c10311a390f9e0030809c092915124e03b69a0
kylejkent <kylejkent@gmail.com>
2022-09-13 14:14:23 +0700

    moved results array to router get

--- controllers/results.js
+++ controllers/results.js
@@ -5,61 +5,68 @@ const router = express.Router()
 // *** FIRST DAY TEST END ***
 // Create POST controller
 // *** FIRST DAY TEST START ***
+
 router.get('/', (req, res) => {
-  res.render('results')
+  // *** RESULTS TEST ARRAY START ***
+  let results = [
+    {
+      title: 'JS tutorials',
+      description: 'The best JavaScript tutorials in the galaxy!',
+      url: 'https://www.w3schools.com',
+      links: [
+        {
+          title: 'JS for Beginners',
+          url: 'https://www.w3schools.com/js'
+        },
+        {
+          title: 'JS for the Web',
+          url: 'https://www.w3schools.com/js'
+        }
+      ]
+    },
+    {
+      title: 'Tortuga Coders',
+      description: 'F&#k it!, Its Real!',
+      url: 'https://www.Tortuga Coders.com',
+      links: [
+        {
+          title: 'JS for Beginners',
+          url: 'https://www.TC.com/js'
+        },
+        {
+          title: 'JS for the Web',
+          url: 'https://www.TCs.com/CSS'
+        }
+      ]
+    },
+    {
+      title: 'Wikipedia',
+      description: 'Wiki is real',
+      url: 'https://www.wikipedia.com',
+      links: [
+        {
+          title: 'JS for Beginners',
+          url: 'https://www.w3schools.com/js'
+        },
+        {
+          title: 'JS for the Web Cool!',
+          url: 'https://www.w3schools.com/js'
+        }
+      ]
+    }
+  ]
+  // *** Test Results qty start ***
+  const resultsLength = results => {
+    let numResults = results.length
+    return numResults
+  }
+  // *** Test Results qty end ***
+
+  // *** RESULTS TEST ARRAY END ***
+  res.render('results', { results })
 })
 // *** FIRST DAY TEST END ***
 // Export module
 // *** FIRST DAY TEST START ***
 module.exports = router
 // *** FIRST DAY TEST END ***
-
-// *** RESULTS TEST ARRAY START ***
-let resultArray = [
-  {
-    title: 'JS tutorials',
-    description: 'The best JavaScript tutorials in the galaxy!',
-    url: 'https://www.w3schools.com',
-    links: [
-      {
-        title: 'JS for Beginners',
-        url: 'https://www.w3schools.com/js'
-      },
-      {
-        title: 'JS for the Web',
-        url: 'https://www.w3schools.com/js'
-      }
-    ]
-  },
-  {
-    title: 'Tortuga Coders',
-    description: 'F&#k it!, Its Real!',
-    url: 'https://www.Tortuga Coders.com',
-    links: [
-      {
-        title: 'JS for Beginners',
-        url: 'https://www.TC.com/js'
-      },
-      {
-        title: 'JS for the Web',
-        url: 'https://www.TCs.com/CSS'
-      }
-    ]
-  },
-  {
-    title: 'Wikipedia',
-    description: 'Wiki is real',
-    url: 'https://www.wikipedia.com',
-    links: [
-      {
-        title: 'JS for Beginners',
-        url: 'https://www.w3schools.com/js'
-      },
-      {
-        title: 'JS for the Web',
-        url: 'https://www.w3schools.com/js'
-      }
-    ]
-  }
-]
-// *** RESULTS TEST ARRAY END ***
