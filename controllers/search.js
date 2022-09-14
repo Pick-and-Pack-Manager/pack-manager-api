de4c84b29caf5affb3ec987f1f86d5a221a1f415
kylejkent <kylejkent@gmail.com>
2022-09-14 13:07:49 +0700

    remove commenting

--- controllers/search.js
+++ controllers/search.js
@@ -5,12 +5,9 @@ const router = express.Router()
 
 // Views
 // Create here a controller that accepts GET requests and renders the "search" page
-
-// *** SEARCH BEFORE CLASS TEST START ***
 router.get('/', (req, res) => {
   res.render('search')
 })
-// *** SEARCH BEFORE CLASS TEST END ***
 
 // Export
 module.exports = router
