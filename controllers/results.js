292e5692f1144a0bd8fc9058ac05db40de6ebfe0
kylejkent <kylejkent@gmail.com>
2022-09-14 16:55:22 +0700

    moved Results to to of page

--- controllers/results.js
+++ controllers/results.js
@@ -1,3 +1,84 @@
+// *** TEST ARRAY START***
+let resultsTest = [
+  {
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js'
+      },
+      {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js'
+      }
+    ]
+  },
+  {
+    title: 'Tortuga Coders',
+    description: 'F&#k it!, Its Real!',
+    url: 'https://tortugacoders.com/',
+    links: [
+      {
+        title: 'Apply',
+        url: 'https://tortugacoders.com/apply'
+      },
+      {
+        title: 'Wed Dev Courses',
+        url: 'https://tortugacoders.com/courses'
+      }
+    ]
+  },
+  {
+    title: 'Wikipedia',
+    description:
+      'JavaScript is a high-level, often just-in-time compiled language that conforms to the ECMAScript standard.',
+    url: 'https://en.wikipedia.org/wiki/JavaScript',
+    links: [
+      {
+        title: 'Javascript Syntax',
+        url: 'https://en.wikipedia.org/wiki/JavaScript_syntax'
+      },
+      {
+        title: 'JS for the Web Cool!',
+        url: 'https://www.w3schools.com/js'
+      }
+    ]
+  },
+  {
+    title: 'Code Wars',
+    description:
+      'Improve your development skills · by training with your peers on code kata that continuously challenge and push your coding practice',
+    url: 'https://www.codewars.com/',
+    links: [
+      {
+        title: 'Log In',
+        url: 'https://www.codewars.com/users/sign_in'
+      },
+      {
+        title: 'Basic Python',
+        url: 'https://www.codewars.com/collections/basic-python'
+      }
+    ]
+  }
+]
+// *** TEST ARRAY END ***
+
+// *** TEST FILTER START ***
+// *** FILTER START ***
+const filterResults = (array, search) => {
+  let newArray = []
+  array.forEach((obj, ind) => {
+    if (obj.title.includes(search)) {
+      newArray.push(obj)
+    }
+  })
+  return newArray
+}
+// *** FILTER END ***
+// *** TEST FILTER END ***
+
 // Import Packages
 const express = require('express')
 const router = express.Router()
@@ -8,73 +89,6 @@ router.post('/', (req, res) => {
 })
 
 router.get('/', (req, res) => {
-  // *** RESULTS TEST ARRAY START ***
-  let results = [
-    {
-      title: 'JS tutorials',
-      description: 'The best JavaScript tutorials in the galaxy!',
-      url: 'https://www.w3schools.com',
-      links: [
-        {
-          title: 'JS for Beginners',
-          url: 'https://www.w3schools.com/js'
-        },
-        {
-          title: 'JS for the Web',
-          url: 'https://www.w3schools.com/js'
-        }
-      ]
-    },
-    {
-      title: 'Tortuga Coders',
-      description: 'F&#k it!, Its Real!',
-      url: 'https://tortugacoders.com/',
-      links: [
-        {
-          title: 'Apply',
-          url: 'https://tortugacoders.com/apply'
-        },
-        {
-          title: 'Wed Dev Courses',
-          url: 'https://tortugacoders.com/courses'
-        }
-      ]
-    },
-    {
-      title: 'Wikipedia',
-      description:
-        'JavaScript is a high-level, often just-in-time compiled language that conforms to the ECMAScript standard.',
-      url: 'https://en.wikipedia.org/wiki/JavaScript',
-      links: [
-        {
-          title: 'Javascript Syntax',
-          url: 'https://en.wikipedia.org/wiki/JavaScript_syntax'
-        },
-        {
-          title: 'JS for the Web Cool!',
-          url: 'https://www.w3schools.com/js'
-        }
-      ]
-    },
-    {
-      title: 'Code Wars',
-      description:
-        'Improve your development skills · by training with your peers on code kata that continuously challenge and push your coding practice',
-      url: 'https://www.codewars.com/',
-      links: [
-        {
-          title: 'Log In',
-          url: 'https://www.codewars.com/users/sign_in'
-        },
-        {
-          title: 'Basic Python',
-          url: 'https://www.codewars.com/collections/basic-python'
-        }
-      ]
-    }
-  ]
-  // *** RESULTS TEST ARRAY END ***
-  res.render('results', { results })
 })
 // Export module
 module.exports = router
