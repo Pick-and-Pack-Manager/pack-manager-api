1733876e6a8f40f6b8664eb8dfa9747820a45914
kylejkent <kylejkent@gmail.com>
2022-09-16 13:51:35 +0700

    add results model

--- models/results.js
+++ models/results.js
@@ -1,3 +1,23 @@
 const mongoose = require('mongoose')
 
 // Create the results moodel
+mongoose.model('results', {
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  links: [
+    {
+      title: String,
+      url: String
+    }
+  ]
+})
