56dbdc2fb226b67c9b69232b4aad193eeefa87df
kylejkent <kylejkent@gmail.com>
2022-09-16 17:20:11 +0700

    search qty

--- controllers/results.js
+++ controllers/results.js
@@ -6,11 +6,29 @@ const Results = require('../models/results.js')
 // Create POST controller
 router.post('/', async (req, res) => {
   console.log(req.body.search)
-  let results = await Results.find({})
+  let results = await Results.find({
+    title: { $regex: req.body.search }
+  })
+  // *** Results Qty ***
+  // const qtyText = async () => {
+  //   let qtyRes = await Results.countDocuments({
+  //     title: { $regex: req.body.search }
+  //   })
+  //   return qtyRes == 1 ? qtyRes + ' Result' : qtyRes + ' Results'
+  // }
+  // *** Results Qty ***
+
+  // *** Search Qty Text ***
+  const qtyText = () => {
+    return results.length == 1
+      ? results.length + ' Result'
+      : results.length + ' Results'
+  }
+  // *** Search Qty Text ***
   console.log(results)
   res.render('results', {
-    // resultsData: filterResults(resultsTest, searched),
-    // resultAmount: resultQty(resultsTest, searched)
+    results,
+    qtyText: qtyText()
   })
 })
 
