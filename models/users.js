521f24deec71541daa425706f800958bdcb3c5b1
kylejkent <kylejkent@gmail.com>
2022-10-03 15:33:53 +0700

    add users model

---
+++ models/users.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+let usersModel = mongoose.model('users', {
+  email: {
+    type: String,
+    required: true
+  },
+	password: {
+    type: String,
+    required: true
+  },
+	firstName: {
+		type: String,
+		required: true
+	},
+	lastName: {
+		type: String,
+		required: true
+	},
+	createdDate: {
+		type: Date,
+		default: Date.now
+	},
+	createdBy: {
+    type: ObjectId,
+    ref: 'users'
+  },
+	permission: {
+		type: String,
+		default: 'A'
+		// A - No Access, B - Basic Access, C - Supervisor. If A or Null no Access
+	},
+
+
+})
+module.exports = usersModel
