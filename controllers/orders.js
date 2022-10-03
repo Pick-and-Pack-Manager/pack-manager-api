24ad39ecf3cd77ff65f53860102db00d0adac775
kylejkent <kylejkent@gmail.com>
2022-10-03 15:56:10 +0700

    setup route structure for orders

--- controllers/orders.js
+++ controllers/orders.js
@@ -8,25 +8,26 @@ const Orders = require('../models/orders.js')
 // const app = express()
 
 // Views
+// Only GET and PATCH allowed. Cannot delete or add
 // *** start GET orders start ***
 router.get('/', async (req, res) => {
   let orders = await Orders.find({
     // title: { $regex: req.query.search || '' }
   })
-	console.log(req.query.search)
+	console.log('GET Orders')
   res.json(orders)
 })
 // *** end GET orders end ***
 
-// *** start POST orders start ***
-router.post('/', async (req, res) => {
+// *** start PATCH orders start ***
+router.patch('/', async (req, res) => {
   let orders = await Orders.find({
     // title: { $regex: req.query.search || '' }
   })
-	console.log(req.query.search)
+	console.log('PATCH Orders')
   res.json(orders)
 })
-// *** end POST orders end ***
+// *** end PATCH orders end ***
 
 // Export module
 module.exports = router
