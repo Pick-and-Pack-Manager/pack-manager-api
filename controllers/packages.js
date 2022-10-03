256a2a89b846467f66cf8d6b690da2112e16828e
kylejkent <kylejkent@gmail.com>
2022-10-03 16:12:28 +0700

    add packages allowed routes comment

--- controllers/packages.js
+++ controllers/packages.js
@@ -8,6 +8,7 @@ const Packages = require('../models/packages.js')
 // const app = express()
 
 // Views
+// *** Packages are a part of this app. Full Control Allowed ***
 // *** start GET Packages start ***
 router.get('/', async (req, res) => {
   let packages = await Packages.find({})
