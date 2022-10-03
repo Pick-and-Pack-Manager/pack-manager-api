4e98e8c2af65ec0472a80b43a3211ebb377d5db6
kylejkent <kylejkent@gmail.com>
2022-10-03 16:03:00 +0700

    create user controller routes
    only get allowed

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -3,31 +3,19 @@ const express = require('express')
 const router = express.Router()
 
 
-const Orders = require('../models/orders.js')
+const Inventory = require('../models/inventory.js')
 
 // const app = express()
 
 // Views
-// Only GET and PATCH allowed. Cannot delete or add
-// *** start GET orders start ***
+// Only GET allowed.
+// *** start GET Inventory start ***
 router.get('/', async (req, res) => {
-  let orders = await Orders.find({
-    // title: { $regex: req.query.search || '' }
-  })
-	console.log('GET Orders')
-  res.json(orders)
+  let inventory = await Inventory.find({})
+	console.log('GET Inventory')
+  res.json(inventory)
 })
-// *** end GET orders end ***
-
-// *** start PATCH orders start ***
-router.patch('/', async (req, res) => {
-  let orders = await Orders.find({
-    // title: { $regex: req.query.search || '' }
-  })
-	console.log('PATCH Orders')
-  res.json(orders)
-})
-// *** end PATCH orders end ***
+// *** end GET Inventory end ***
 
 // Export module
 module.exports = router
