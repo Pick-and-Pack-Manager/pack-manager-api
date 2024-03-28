f1279a141a09bc6a64ad513c87d265d5f703dda2
kylejkent <kylejkent@gmail.com>
2024-03-28 15:54:51 +0800

    inventory query

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
 	console.log('Inventory Query connecting...')
       // Construct SQL query to fetch conversion data with necessary joins
       const query = `
-			Select T0.ItemCode, T1.itmsGrpNam, T0.itemName, T2.WhsCode, T2.OnHand, Isnull(T2.StockValue,0) From OITM T0 inner join OITB T1 on T1.itmsGrpCod = T0.itmsGrpCod inner join OITW T2 on T2.ItemCode = T0.ItemCode where T2.OnHand <> 0 order by T2.WhsCode, T2.ItemCode
+			Select T0.ItemCode, T1.itmsGrpNam, T0.itemName, T2.WhsCode, T2.OnHand, Isnull(T2.StockValue,0) AS StockValue From OITM T0 inner join OITB T1 on T1.itmsGrpCod = T0.itmsGrpCod inner join OITW T2 on T2.ItemCode = T0.ItemCode where T2.OnHand <> 0 order by T2.WhsCode, T2.ItemCode
 			`;
 			// Execute the query
 			odbc.connect(process.env.ODBC_SAPB1, async () => {console.log('Connecting to SAP B1 ODBC')
@@ -28,8 +28,8 @@ router.get('/', async (req, res) => {
 			console.log('inventory query ran!!!')
 			console.log(inventoryData)
 			console.log('Inventory Query Complete!!!')
+			  res.json(inventoryData)
 			});
-  res.json(query)
 })
 // *** end GET Inventory end ***
 
