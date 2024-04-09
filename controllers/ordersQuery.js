fb1f6faa5e91745151f114f6f7d54885ab8584ac
kylejkent <kylejkent@gmail.com>
2024-04-09 22:13:09 +0800

    Create ordersQuery.js

---
+++ controllers/ordersQuery.js
@@ -0,0 +1,69 @@
+// Import Packages
+const express = require('express')
+const router = express.Router()
+const moment = require('moment')
+const odbc = require('odbc');
+
+// Database configuration
+const sapB1ODBC = process.env.ODBC_SAPB1
+
+// Views
+// *** Orders are pulled from external source (SAP B1 in this case) so limited updating allowed  Only Issued, IssuedQty, IssuedDate, IssuedBalance, FreeText, PackageID, ManifaestID***
+// *** start GET orders start ***
+router.post('/', async (req, res, next) => {
+	try {
+		if (req.isAuthenticated()) {
+			console.log(req.body)
+			// DELETE NULL FROM body
+			if (req.body.kittingRoute == "all" || req.body.kittingRoute == null || req.body.kittingRoute == undefined) {
+				delete req.body.kittingRoute
+			}
+			// DELETE NULL FROM body
+			if (req.body.completingRoute == "all" || req.body.completingRoute == null || req.body.completingRoute == undefined) {
+				delete req.body.completingRoute
+			}
+			// DELETE NULL FROM body
+			if (req.body.despatchRoute == "all" || req.body.despatchRoute == null || req.body.despatchRoute == undefined || req.body.despatchRoute == 'DESP1') {
+				delete req.body.despatchRoute
+			}
+			if (req.body.kittingDate == "all" || req.body.kittingDate == null || req.body.kittingDate == undefined) {
+				delete req.body.kittingDate
+			} else {
+      req.body.kittingDate = {
+				$gte: moment(req.body.kittingDate).format('YYYY-MM-DD'),
+				$lt: moment(req.body.kittingDate).add(1, "days").format('YYYY-MM-DD')
+			}
+		}	if (req.body.completingDate == "all" || req.body.completingDate == null || req.body.completingDate == undefined) {
+						delete req.body.completingDate
+					} else {
+						req.body.completingDate = {
+							$gte: moment(req.body.completingDate).format('YYYY-MM-DD'),
+							$lt: moment(req.body.completingDate).add(1, "days").format('YYYY-MM-DD')
+						}
+		}	if (req.body.despatchDate == "all" || req.body.despatchDate == null || req.body.despatchDate == undefined) {
+						delete req.body.despatchDate
+					} else {
+						req.body.despatchDate = {
+							$gte: moment(req.body.despatchDate).format('YYYY-MM-DD'),
+							$lt: moment(req.body.despatchDate).add(1, "days").format('YYYY-MM-DD')
+						}
+				}
+			let orders = await Orders.find(req.body)
+			console.log(orders)
+			let sortedOrders = orders.sort((a, b) => new Date(a.docDueDate) - new Date(b.docDueDate))
+			console.log(orders)
+
+			res.json(sortedOrders)
+		} else {
+			console.log('User not logged in')
+			res.json({errorMessage: 'LOGGED IN USER NOT AUTHENTICATED!!! Log out and try again'})
+			throw new Error('Not Logged In')
+		}
+
+
+	} catch (err) {next(err)}
+})
+// *** end GET orders end ***
+
+// Export module
+module.exports = router
